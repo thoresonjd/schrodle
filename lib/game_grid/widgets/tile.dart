72b2d0e928ef3e5b24b3924dac3de36dc0ec6217
thoresonjd <thoresonjd@gmail.com>
2024-06-01 18:04:12 -0700

    Refactor _colorFromStatus method into tileStatusColors map

--- lib/game_grid/widgets/tile.dart
+++ lib/game_grid/widgets/tile.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:schrodle/game_grid/bloc/game_grid_bloc.dart';
 import 'package:schrodle/game_grid/data/tile_status.dart';
+import 'package:schrodle/game_grid/data/tile_status_colors.dart';
 import 'package:schrodle/theme/theme.dart';
 
 /// {@template tile}
@@ -38,23 +39,6 @@ class Tile extends StatefulWidget {
 }
 
 class _TileState extends State<Tile> with AutomaticKeepAliveClientMixin {
-  /// Retrieves the [Color] for the corresponding [TileStatus].
-  static Color _colorFromStatus({required TileStatus status}) {
-    switch (status) {
-      case TileStatus.guessed:
-        return SchrodleColors.guessed;
-      case TileStatus.correct:
-        return SchrodleColors.correct;
-      case TileStatus.present:
-        return SchrodleColors.present;
-      case TileStatus.absent:
-        return SchrodleColors.absent;
-      case TileStatus.occupied:
-      case TileStatus.unoccupied:
-        return SchrodleColors.unevaluated;
-    }
-  }
-
   @override
   bool get wantKeepAlive => true;
 
@@ -71,7 +55,7 @@ class _TileState extends State<Tile> with AutomaticKeepAliveClientMixin {
         final tileLetter = tile == null ? '' : tile.letter;
         final color = tile == null
             ? SchrodleColors.unevaluated
-            : _colorFromStatus(status: tile.status);
+            : tileStatusColors[tile.status];
         final borderColor =
             tileStatus == null || tileStatus == TileStatus.unoccupied
                 ? SchrodleColors.highlight
@@ -97,7 +81,7 @@ class _TileState extends State<Tile> with AutomaticKeepAliveClientMixin {
             ),
           ),
           back: ColoredBox(
-            color: color,
+            color: color!,
             child: FittedBox(
               child: Text(
                 tileLetter,
